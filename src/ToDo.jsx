765ee6ad8884306e6ecb3e8caf33d8f7239b2e36
Rahnas p <rahnasx2@gmail.com>
2024-12-15 15:14:35 +0530

    todo-react

---
+++ src/ToDo.jsx
@@ -0,0 +1,132 @@
+import { useState } from "react"
+
+function ToDo() {
+
+    const [newTask, setNewTask] = useState()   //handling input data 
+
+    const [task, addTask] = useState([])      //for adding tasks to the list
+
+    const [editTask, setEditTask] = useState(false)     //for performing edit 
+
+    const [editTaskIndex, setEditTaskIndex] = useState(null)
+
+    const [completedTask, setCompletedTask] = useState([])
+
+    const [isComplete, setIsComplete] = useState(false)
+
+
+    //handle the input change
+    const handleNewTask = (e) => {
+        setNewTask(e.target.value)
+    }
+
+    //adding new task
+    const handleTask = () => {
+        if (!newTask) return
+        if (task.includes(newTask)) return alert('alrealy exist in your list')
+        addTask([...task, newTask])
+        setNewTask('')
+        setIsComplete(false)
+    }
+
+    //delete task
+    const handleDelete = (i) => {
+        alert('are you sure')
+        let afterRemovel = task.filter((_, index) => index !== i)
+        addTask(afterRemovel)
+    }
+
+
+    //edit a task
+    const handleEdit = (i) => {
+        setEditTask(true)
+        setNewTask(task[i])
+        setEditTaskIndex(i)
+    }
+
+    //updadte the task
+    const handleUpdate = () => {
+        if (!newTask) return
+        if (task.includes(newTask)) return alert('alrealy exist in your list')
+
+        let updatedTask = [...task]
+
+        // if(updatedTask[editTaskIndex] !== newTask)  {
+        //     updatedTask[editTaskIndex] = newTask
+        //     addTask(updatedTask)
+        // }  
+
+        updatedTask[editTaskIndex] = newTask
+        addTask(updatedTask)
+
+        setEditTask(false)
+        setNewTask('')
+        editTaskIndex(null)
+
+
+    }
+
+    //reverse the order 
+    const handleSort = () => {
+        task.reverse()
+        addTask([...task])
+    }
+
+    //handle completed task
+    const handleCompletedTask = (i) =>{
+        let currentTask = task[i]
+
+        if(completedTask.includes(currentTask)){
+            setCompletedTask(completedTask.filter(existedTask => existedTask !== currentTask))
+        }else{
+            setCompletedTask([...completedTask,task[i]])
+        }
+
+    }
+
+    return (
+        <div className="todo-container">
+            <div>
+                <h3 className="todo-tittle">STAY ON TRACK</h3>
+                <div className="input-container">
+                    <input onChange={handleNewTask} value={newTask} type="text" placeholder="enter your task" />
+                    {editTask ? <button onClick={handleUpdate} className="add-btn">Update</button> :
+                        <button onClick={handleTask} className="add-btn">Add</button>
+                    }
+                    <span onClick={handleSort} className="sort-list"><i class="fa-solid fa-sort"></i></span>
+                </div>
+
+
+                {isComplete ?
+                    <ul className="todo-list">
+                        {completedTask.map((t, i) => (
+                            <li key={i} className="list-items">
+                                {t}
+                            </li>
+                        ))}
+                        <button className="back-view-btn" onClick={() => setIsComplete(false)}>BACK</button>
+                    </ul> :
+                    <ul className="todo-list">
+                        {task.map((t, i) => (
+                            <li key={i} className="list-items">
+                                <input type="checkbox" 
+                                checked={completedTask.includes(t)}
+                                onClick={() => handleCompletedTask(i)}/>
+                                {t}
+                                <button onClick={() => handleEdit(i)}>
+                                    <i class="fas fa-edit"></i></button>
+                                <button onClick={() => handleDelete(i)}>
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                </button>
+                            </li>
+                        ))}
+                        {completedTask.length > 0 ? <button onClick={() => setIsComplete(true)} className="back-view-btn">VIEW COMPLETED TASK</button>: ''}
+                    </ul>      
+                } 
+                
+            </div>
+        </div>
+    )
+}
+
+export default ToDo
